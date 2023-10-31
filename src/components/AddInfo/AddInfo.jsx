81bf70936442f3f2f8359d1f246bd7d700555679
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-10-31 20:14:26 +0100

    fixed backlink

--- src/components/AddInfo/AddInfo.jsx
+++ src/components/AddInfo/AddInfo.jsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet, useParams } from 'react-router-dom';
+import { NavLink, Outlet, useLocation, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { ListAddInfoStyled } from './AddInfo.styled';
 
@@ -9,16 +9,26 @@ const StyledLink = styled(NavLink)`
 `;
 export const AddInfo = () => {
   const param = useParams();
+  const location = useLocation();
+  const cameFrom = location?.state?.from ?? '/';
 
   return (
     <>
       Additional information
       <ListAddInfoStyled>
         <li>
-          <StyledLink to={`/movies/${param.movieById}/cast`}>Cast</StyledLink>
+          <StyledLink
+            to={`/movies/${param.movieById}/cast`}
+            state={{ from: cameFrom }}
+          >
+            Cast
+          </StyledLink>
         </li>
         <li>
-          <StyledLink to={`/movies/${param.movieById}/reviews`}>
+          <StyledLink
+            to={`/movies/${param.movieById}/reviews`}
+            state={{ from: cameFrom }}
+          >
             Reviews
           </StyledLink>
         </li>
