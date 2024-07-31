c52be5f88a9726c78ad61693f7670cef4c0167c2
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2024-07-31 12:55:26 +0200

    change link, readme file

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const theme = {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/goit-react-hw-05-moviess">
+      <BrowserRouter basename="/movies">
         <App />
         <GlobalStyle />
       </BrowserRouter>
